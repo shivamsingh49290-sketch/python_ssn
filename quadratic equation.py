35299f35bfa02bba094bf7edb6348a1b18e71e9f
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-10 18:36:21 +0530

    quadratic equation

---
+++ quadratic equation.py
@@ -0,0 +1,11 @@
+a=float(input("enter a number"))
+b=float(input("enter a number"))
+c=float(input("enter a number"))
+import cmath as m
+d=(b**2)-(4*a*c)
+print("d=" ,d)
+x1=((-b + m.sqrt(d))/(2*a))
+x2=((-b - m.sqrt(d))/(2*a))
+print("x1=" ,x1)
+print("x2=" ,x2)
+
