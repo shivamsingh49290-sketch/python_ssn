fe4dd28c1023fcb6326f81313f30a81932fc3680
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-10 12:10:14 +0530

    greatest of 3 number

---
+++ greatest of 3 number.py
@@ -0,0 +1,28 @@
+# to find greatest of 3 number
+
+
+# way1
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+
+if a>b and a>c:
+    print("a is greatest")
+elif b>a and b>c:
+    print("b is greatest")
+else:
+    print("c is greatest")
+
+
+# way2
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+c=int(input("Enter a number:"))
+if a>b:
+    print("a is greatest")
+elif a>c:
+    print("a is greatest")
+elif b>c:
+    print("b is greatest")
+else:
+    print("c is greatest")
