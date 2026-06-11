213253fd1774af17c6188d83c6426cc51c594903
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-11 21:48:40 +0530

    quadratic equation

--- quadratic equation.py
+++ quadratic equation.py
@@ -1,25 +1,16 @@
-'''
-#way1
-a=float(input("enter a number"))
-b=float(input("enter a number"))
-c=float(input("enter a number"))
-import cmath  as m
-d=(b**2)-(4*a*c)
-print("d=" ,d)
-x1=((-b + m.sqrt(d))/(2*a))
-x2=((-b - m.sqrt(d))/(2*a))
-print("x1=" , x1)
-print("x2=" , x2)
 
-'''
-# way2 -
 a=float(input("enter a number"))
 b=float(input("enter a number"))
 c=float(input("enter a number"))
 import math  as m
+import cmath as cm
 d=(b*b)-(4*a*c)
 if d<0:
     print("no real roots")
+    x1 = ((-b + cm.sqrt(d)) / (2 * a))
+    x2 = ((-b - cm.sqrt(d)) / (2 * a))
+    print("x1=", x1)
+    print("x2=", x2)
 elif d==0:
     print("real and equal roots")
     x1=x2=-b/(2*a)
