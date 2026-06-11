821e11a3129e1129804a71d2f27fcc35895644e1
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-11 10:56:39 +0530

    quadratic equation

--- quadratic equation.py
+++ quadratic equation.py
@@ -1,4 +1,4 @@
-
+'''
 #way1
 a=float(input("enter a number"))
 b=float(input("enter a number"))
@@ -11,7 +11,7 @@ x2=((-b - m.sqrt(d))/(2*a))
 print("x1=" , x1)
 print("x2=" , x2)
 
-
+'''
 # way2 -
 a=float(input("enter a number"))
 b=float(input("enter a number"))
@@ -22,6 +22,8 @@ if d<0:
     print("no real roots")
 elif d==0:
     print("real and equal roots")
+    x1=x2=-b/(2*a)
+    print("x1=" , x1 , "x2=" , x2)
 else:
     x1=((-b + m.sqrt(d))/(2*a))
     x2=((-b - m.sqrt(d))/(2*a))
