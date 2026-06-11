2f93657e7bfdf71c0ca465c9b400d636a0d3642f
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-11 11:17:48 +0530

    grade of students

--- grade of student.py
+++ grade of student.py
@@ -3,15 +3,22 @@ b=int(input("Enter a number"))
 c=int(input("Enter a number"))
 d=int(input("Enter a number"))
 e=int(input("Enter a number"))
-sum=a+b+c+d+e
-print(sum)
-percentage=(sum/500*100)
-print(percentage)
-if percentage<33:
-    print("fail")
-elif percentage<=33 and percentage>45:
-    print("third division")
-elif percentage<=45 and percentage>60:
-    print("second division")
+if a<=100 and b<=100 and c<=100 and d<=100 and e<=100:
+    if a>=0 and b>=0 and c>=0 and d>=0 and e>0:
+        sum = a + b + c + d + e
+        print(sum)
+        percentage = (sum / 500 * 100)
+        print(percentage)
+        if percentage < 33:
+            print("fail")
+        elif percentage <= 33 and percentage > 45:
+            print("third division")
+        elif percentage <= 45 and percentage > 60:
+            print("second division")
+        else:
+            print("first division")
+
+    else:
+        print("check the entered number is in negative ")
 else:
-    print("first division")
+    print("check the entered number exceeds more than 100 ")
