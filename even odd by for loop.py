8f4b1223da76b7dc423d53c8cb8b302e70acddb0
shivamsingh49290-sketch <shivamsingh49290@gmail.com>
2026-06-13 12:01:45 +0530

    even odd by for loop

---
+++ even odd by for loop.py
@@ -0,0 +1,12 @@
+n=int(input("enter the starting number"))
+m=int(input("enter the ending number"))
+for i in range(n,m):
+    if i%2==0:
+        print()
+    else:
+        print(i)
+for i in range(n,m,-1):
+    if i%2==0:
+        print()
+    else:
+        print(i)
